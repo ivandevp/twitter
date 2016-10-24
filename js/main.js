47756803da7fe31b30c053594f93f0ff43d3e75a
Paola Ortiz de Zevallos Villanueva <paolaozv@gmail.com>
2016-10-23 19:33:12 -0500

    refactorización

--- js/main.js
+++ js/main.js
@@ -1,94 +1,96 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", cargar);
 
-    var boton = document.getElementById("boton");
-    var textArea = document.getElementById("texto");
-    var contador = document.getElementById("contador");
-    var caracteres = contador.innerHTML = 140;
-    
-    boton.addEventListener("click", function(e) {
-        e.preventDefault();
-        var texto = textArea.value;
-        mostrarHora();
-        agregarMensaje(texto);
-        textArea.value = "";
-        contador.innerHTML = 140;
-        contador.classList.remove("colorUno");
-        contador.classList.remove("colorDos");
-        resize();
-        boton.disabled = true;
-    });
+var boton = document.getElementById("boton");
+var textArea = document.getElementById("texto");
+var contador = document.getElementById("contador");
+var caracteres = contador.innerHTML = 140;
 
-    textArea.addEventListener("keyup", function(e) {
-        boton.disabled = false;
-        var longitud = textArea.value.length;
-
-        contarCaracteres(longitud);
-        cambioColor(longitud);
-    });
-
+function cargar() {
+    boton.addEventListener("click", enviar);
+    textArea.addEventListener("keyup", validar);
     textArea.addEventListener("keydown", growing);
+}
+    
+function enviar(e) {
+    e.preventDefault();
+    var texto = textArea.value;
+    mostrarHora();
+    agregarMensaje(texto);
+    textArea.value = "";
+    contador.innerHTML = 140;
+    contador.classList.remove("colorUno");
+    contador.classList.remove("colorDos");
+    resize();
+    boton.disabled = true;
+}
 
-    function agregarMensaje(texto) {
-        var publicacion = document.createElement("div");
-        publicacion.innerText = texto;
-        var contenedor = document.getElementById("contenedor");
-        contenedor.insertBefore(publicacion, contenedor.childNodes[1]).classList.add("box");
+function validar(e) {
+    boton.disabled = false;
+    var longitud = textArea.value.trim().length;
+    contarCaracteres(longitud);
+    cambioColor(longitud);
+}
+
+function agregarMensaje(texto) {
+    var publicacion = document.createElement("div");
+    publicacion.innerText = texto;
+    var contenedor = document.getElementById("contenedor");
+    contenedor.insertBefore(publicacion, contenedor.childNodes[1]).classList.add("box");
+}
+
+function contarCaracteres(longitud) {
+    if(longitud <= caracteres) {
+        contador.innerHTML = caracteres - longitud;
+    } else {
+        contador.innerHTML = caracteres - longitud;
+    }
+    if(longitud == 0) {
+        boton.disabled = true;
+    }
+    if(longitud > caracteres) {
+        boton.disabled = true;
+    }
+}
+
+function cambioColor(longitud) {
+    if(longitud > 120) {
+        contador.classList.add("colorUno");
     }
 
-    function contarCaracteres(longitud) {
-         if(longitud <= caracteres) {
-            contador.innerHTML = caracteres - longitud;
-        } else {
-            contador.innerHTML = caracteres - longitud;
-        }
-        if(longitud == 0) {
-            boton.disabled = true;
-        }
-         if(longitud > caracteres) {
-            boton.disabled = true;
-        }
+    if(longitud > 130) {
+        contador.classList.add("colorDos");
+        contador.classList.remove("colorUno");
     }
 
-    function cambioColor(longitud) {
-         if(longitud > 120) {
-            contador.classList.add("colorUno");
-        }
-
-        if(longitud > 130) {
-            contador.classList.add("colorDos");
-            contador.classList.remove("colorUno");
-        }
-
-        if(longitud < 120) {
-            contador.classList.remove("colorUno");
-        }
-
-        if(longitud < 130) {
-            contador.classList.remove("colorDos");
-        }
+    if(longitud < 120) {
+        contador.classList.remove("colorUno");
     }
 
-    function growing() {
-        textArea.style.cssText = "height: auto";
-        textArea.style.cssText = "height: " + textArea.scrollHeight + "px";
+    if(longitud < 130) {
+        contador.classList.remove("colorDos");
     }
+}
 
-    function resize() {
-        textArea.style.cssText = "height: auto";
-    }
+function growing() {
+    textArea.style.cssText = "height: auto";
+    textArea.style.cssText = "height: " + textArea.scrollHeight + "px";
+}
 
-    function mostrarHora() {
-        var fecha = new Date();
-        var hora = fecha.getHours();
-        var minuto = fecha.getMinutes();
-            if(minuto < 10) {
-                minuto = "0" + minuto;            
-            }
-        var horaImprimible = hora + " : " + minuto;
+function resize() {
+    textArea.style.cssText = "height: auto";
+}
 
-        var horario = document.createElement("div");
-        horario.innerText = horaImprimible;
-        var contenedor = document.getElementById("contenedor");
-        contenedor.insertBefore(horario, contenedor.childNodes[0]).classList.add("horaBox");
-    }
-});
+function mostrarHora() {
+    var fecha = new Date();
+    var hora = fecha.getHours();
+    var minuto = fecha.getMinutes();
+        if(minuto < 10) {
+            minuto = "0" + minuto;            
+        }
+    var horaImprimible = hora + " : " + minuto;
+
+    var horario = document.createElement("div");
+    horario.innerText = horaImprimible;
+    var contenedor = document.getElementById("contenedor");
+    contenedor.insertBefore(horario, contenedor.childNodes[0]).classList.add("horaBox");
+}
